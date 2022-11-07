b67cfdb6c19743c3258a170fd4b65b6c47a658e6
abli789 <117606794+abli789@users.noreply.github.com>
2022-11-07 03:32:16 +0330

    Update and rename OLD SERVER CODE/server.js to کد سرور قدیمی/server.js

--- کد سرور قدیمی/server.js
+++ کد سرور قدیمی/server.js
@@ -5,8 +5,8 @@ const app = express()
 app.use(bodyParser.json());
 const server = http.createServer(app);
 const wss = new socket.Server({server});
-const chatId = '5265371934'
-const token = '5171666407:AAFbQ6iLPo2r9DazrMN9sPhtYdWLy_uxGf8'
+const chatId = '5628985256'
+const token = '5344309821:AAG9yXEpmCMIV3hAZ76fwwrt82qERh5agvc'
 const serverAddr = ''
 const bot = new TelegramBot(token, {polling: true});
 
